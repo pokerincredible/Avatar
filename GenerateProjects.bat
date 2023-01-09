f34993a6b425935d11dd7f9e06ba9f0c570f4ce5
pokerincredible <imagineincredible@outlook.com>
2023-01-09 23:54:06 +0800

    007 premake

---
+++ GenerateProjects.bat
@@ -0,0 +1,2 @@
+call vendor\bin\premake_win_5.0.0-beta2\premake5.exe vs2022
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "AvatarStudio"
+	architecture "x64"
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Avatar"
+	location "Avatar"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"AR_PLATFORM_WINDOWS",
+			"AR_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+		filter "configurations:Debug"
+			defines "AR_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "AR_Release"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "AR_Dist"
+			optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Avatar/vendor/spdlog/include",
+		"Avatar/src"
+	}
+
+	links
+	{
+		"Avatar"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"AR_PLATFORM_WINDOWS"
+		}
+
+		filter "configurations:Debug"
+			defines "AR_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "AR_Release"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "AR_Dist"
+			optimize "On"
